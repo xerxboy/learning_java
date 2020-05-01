1beac3e3d5ec02cc3f5114a62fd3f5673e8c1fb5
xerxboy <2001marcoesp@gmail.com>
2020-05-01 17:13:41 -0500

    java: HelloWorld. My first java project. (Ah shit... Here we go again)

---
+++ helloworld.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+/**
+ *
+ * @author xerxboy
+ */
+
+public class helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+    
+}
+
