192e72849d7deb8d59e1ae7d7da54734faef50d5
xerxboy <2001marcoesp@gmail.com>
2020-05-01 18:59:50 -0500

    java: String no_primitive = "types_of_data";
    /**
    We can use these types of data:
    
    For numbers:
    Name     Size    =======================Range=======================
    Integer= 32 bits  -2,147,48,648 to 2,147,483,647
    
    NOTES:
    Integer can save the "null" declaration.
    (Null it's a memory space without data).
    
    Integer can use methods.
    
    For characters:
    Name   ==============Utility==============
    String =  You can create a character string.
    Example= String example = "Hello.";
    
    NOTE: YOU CAN´T USE ' ' for this. YOU SHOULD use only " ".
    
    */

---
+++ noprimitivetypesofdata.java
@@ -0,0 +1,38 @@
+package noprimitivetypesofdata;
+
+/**
+ *
+ * @author xerxboy
+ */
+
+
+/**
+We can use these types of data:
+
+For numbers:
+Name     Size    =======================Range=======================
+Integer= 32 bits  -2,147,48,648 to 2,147,483,647
+
+NOTES: 
+Integer can save the "null" declaration. 
+(Null it's a memory space without data).
+
+Integer can use methods. 
+
+For characters: 
+Name   ==============Utility==============
+String =  You can create a character string.
+Example= String example = "Hello.";
+
+NOTE: YOU CAN´T USE ' ' for this. YOU SHOULD use only " ".
+
+*/
+
+public class noprimitivetypesofdata {
+
+public static void main(String[] args) {
+
+   String example = "Hello.";
+   System.out.println(example);
+ }
+}
