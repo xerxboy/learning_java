b8034e0a495fa5ae152e647212037a95d5a547d5
xerxboy <2001marcoesp@gmail.com>
2020-05-01 17:23:12 -0500

    java: /* Comments are so coool */

---
+++ comments.java
@@ -0,0 +1,17 @@
+package comments;
+
+/**
+ *
+ * @author xerxboy
+ */
+
+public class comments {
+  
+  public static void main(String[] args) {
+
+   /** Hello. This is my test-comment. 
+   First comment.
+  */
+  
+ }
+}
